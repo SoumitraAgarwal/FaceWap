906ca577258ce056129c56a97a3126b7c675e648
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-09 05:32:38 +0530

    Working 2 face swap same image

--- Faceswap.py
+++ Faceswap.py
@@ -135,16 +135,23 @@ if(len(rects)==2):
 	landmarks2 = numpy.matrix([[p.x, p.y] for p in predictor(im1, rects[1]).parts()])
 	M = transformation_from_points(landmarks1[ALIGN_POINTS],
 	                               landmarks2[ALIGN_POINTS])
-
-	mask = get_face_mask(im2, landmarks2)
+	M1 = transformation_from_points(landmarks2[ALIGN_POINTS],
+	                               landmarks1[ALIGN_POINTS])
+	mask = get_face_mask(im1, landmarks1)
+	mask1 = get_face_mask(im2, landmarks2)
 	warped_mask = warp_im(mask, M, im1.shape)
+	warped_mask1 = warp_im(mask1, M1, im2.shape)
 	combined_mask = numpy.max([get_face_mask(im1, landmarks1), warped_mask],
 	                          axis=0)
-
+	combined_mask1 = numpy.max([get_face_mask(im2, landmarks2), warped_mask1],
+	                          axis=0)
 	warped_im2 = warp_im(im2, M, im1.shape)
+	warped_im1 = warp_im(im1, M1, im2.shape)
 	warped_corrected_im2 = correct_colours(im1, warped_im2, landmarks1)
-
+	warped_corrected_im1 = correct_colours(im2, warped_im1, landmarks2)
 	output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
+	output_im = output_im * (1.0 - combined_mask1) + warped_corrected_im1 * combined_mask1
+	
 
 	cv2.imwrite('output3.jpg', output_im)
 else:

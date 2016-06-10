200b2d8e6d83f48e2de5ef356131df25dae56882
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-10 07:32:43 +0530

    Remove second transform

--- Faceswap_webcam_image.py
+++ Faceswap_webcam_image.py
@@ -121,6 +121,7 @@ cam.set(3,640)
 cam.set(4,480)
 video_capture = cam
 im2 = cv2.imread("img1.jpg")
+im2 = cv2.resize(im2,(640,480), interpolation = cv2.INTER_CUBIC)
 rects = detector(im2,1)
 landmarks2 = numpy.matrix([[p.x, p.y] for p in predictor(im2, rects[0]).parts()])
 while True:
@@ -134,22 +135,13 @@ while True:
             landmarks1 = numpy.matrix([[p.x, p.y] for p in predictor(im1, rects[0]).parts()])
             M = transformation_from_points(landmarks1[ALIGN_POINTS],
                                                landmarks2[ALIGN_POINTS])
-            M1 = transformation_from_points(landmarks2[ALIGN_POINTS],
-                                           landmarks1[ALIGN_POINTS])
             mask = get_face_mask(im1, landmarks1)
-            mask1 = get_face_mask(im2, landmarks2)
             warped_mask = warp_im(mask, M, im1.shape)
-            warped_mask1 = warp_im(mask1, M1, im2.shape)
             combined_mask = numpy.max([get_face_mask(im1, landmarks1), warped_mask],
                                       axis=0)
-            combined_mask1 = numpy.max([get_face_mask(im2, landmarks2), warped_mask1],
-                                      axis=0)
             warped_im2 = warp_im(im2, M, im1.shape)
-            warped_im1 = warp_im(im1, M1, im2.shape)
             warped_corrected_im2 = correct_colours(im1, warped_im2, landmarks1)
-            warped_corrected_im1 = correct_colours(im2, warped_im1, landmarks2)
-            output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
-            im1 = output_im * (1.0 - combined_mask1) + warped_corrected_im1 * combined_mask1
+            im1 = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
         else:
             print("Insufficient faces")	
 

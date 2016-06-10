cd54c3425561cdf0a9433432feeff40de462a1d7
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-10 08:36:22 +0530

    Add landmarks 2

--- Faceswap_webcam_image.py
+++ Faceswap_webcam_image.py
@@ -129,19 +129,20 @@ while True:
     ret, im1 = video_capture.read()
     if ret:
         rects = detector(im1, 1)
-        for i in range (len(rects)):
-            cv2.rectangle(im1, (rects[0].left(),rects[0].top()),(rects[0].right(),rects[0].bottom()), (0,0,255),2)
+        landmarks1 = numpy.matrix([[p.x, p.y] for p in predictor(im1, rects[0]).parts()])
         if(len(rects)==1):
-            landmarks1 = numpy.matrix([[p.x, p.y] for p in predictor(im1, rects[0]).parts()])
             M = transformation_from_points(landmarks1[ALIGN_POINTS],
-                                               landmarks2[ALIGN_POINTS])
-            mask = get_face_mask(im1, landmarks1)
+                               landmarks2[ALIGN_POINTS])
+
+            mask = get_face_mask(im2, landmarks2)
             warped_mask = warp_im(mask, M, im1.shape)
             combined_mask = numpy.max([get_face_mask(im1, landmarks1), warped_mask],
                                       axis=0)
+
             warped_im2 = warp_im(im2, M, im1.shape)
             warped_corrected_im2 = correct_colours(im1, warped_im2, landmarks1)
-            im1 = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
+
+            output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
         else:
             print("Insufficient faces")	
 
